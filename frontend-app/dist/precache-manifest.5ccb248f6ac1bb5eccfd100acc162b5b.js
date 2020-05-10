7c0620ff100e6baf6038ca3703fdecfd1e48cbd2
fingerpich <zarei.bs@gmail.com>
2020-05-10 17:37:05 +0430

    add dist for gh-page

---
+++ frontend-app/dist/precache-manifest.5ccb248f6ac1bb5eccfd100acc162b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dfd9597431dabcd3c1",
+    "url": "/css/app.fd895c46.css"
+  },
+  {
+    "revision": "9fe95834ba28c395f4e0dbaf21015426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16dfd9597431dabcd3c1",
+    "url": "/js/app.d8a35961.js"
+  },
+  {
+    "revision": "48c7168b6e5743c18522",
+    "url": "/js/chunk-vendors.7d6c1bcf.js"
+  },
+  {
+    "revision": "3efdd7351ce131204f4b90fc7ccad494",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
